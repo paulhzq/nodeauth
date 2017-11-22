88f85a2e442c9026884b5ac8a8963eaf13330e40
Paul Huang <paul_hzq@hotmail.com>
2017-11-22 14:40:15 -0600

    setup the application middleware

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var expressValidator = require('express-validator');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 var bodyParser = require('body-parser');
+var multer = require('multer');
+var flash = require('connect-flash');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var db = mongoose.connection;
+
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -14,13 +24,52 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// Handle File Uploads
+var upload = multer({ dest: './uploads' });
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+//Handle Express Sessions
+app.use(session({
+  secret:'secret',
+  saveUninitialized: true,
+  resave:true
+}));
+
+//passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+//validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+    var namespace = param.split('.')
+    , root = namspace.shift()
+    , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namspace.shift() +']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(flash());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
 
 app.use('/', index);
 app.use('/users', users);
