5213370fee558ab83fdb732e658cca21f51c5d98
Paul Huang <paul_hzq@hotmail.com>
2017-12-18 18:12:41 -0500

    user bcrypt to encrypt the password store

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
 
 mongoose.connect('mongodb://localhost/nodeauth');
 var db = mongoose.connection;
@@ -10,7 +11,7 @@ var UserSchema = mongoose.Schema({
         index: true
     },
     password: {
-        type: String
+        type: String, required:true, bcrypt: true
     },
     email: {
         type: String
@@ -26,6 +27,10 @@ var UserSchema = mongoose.Schema({
 var User = module.exports = mongoose.model('User',UserSchema);
 
 module.exports.createUser = function(newUser, callback){
-    newUser.save(callback);
+    bcrypt.hash(newUser.password, 10, function(err, hash){
+        if(err) throw err;
+        newUser.password = hash;
+        newUser.save(callback);
+    });
 
 }
